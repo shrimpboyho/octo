c12dfef0ac8955f491f11c6dfc6b74cd359d29a0
shrimpboyho <shrimpboyhoftw@gmail.com>
2013-08-11 21:58:44 -0400

    len() works

--- octo.h
+++ octo.h
@@ -1,5 +1,13 @@
 #ifndef OCTO_H
 #define OCTO_H
 
+int len(char* string){
+	int i = 0;
+	while(*string != '\0'){
+		i++;
+		*string++;
+	}
+	return i;
+}
 
 #endif /* OCTO_H */
--- unittests.c
+++ unittests.c
@@ -5,6 +5,8 @@ int main( void ){
 
 	printf("Unit testing has begun\n");
 	/* len() test */
-	
+	char* testy = "BUNZ! ";
+	printf("The string \"%s\" is %d characters long.\n", testy, len(testy));
+	if(len(testy) == 6) printf("TEST PASSED.");
 	return 0;
 }
