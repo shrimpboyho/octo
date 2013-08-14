b8d107ea67065af6fa0fe84b5c0583d4af391a1f
shrimpboyho <shrimpboyhoftw@gmail.com>
2013-08-14 11:27:21 -0400

    Added a leading whitespace strip function

--- octo.h
+++ octo.h
@@ -2,12 +2,27 @@
 #define OCTO_H
 
 int len(char* string){
+	
 	int i = 0;
 	while(*string != '\0'){
 		i++;
 		*string++;
 	}
 	return i;
+
+}
+
+char* strip(char* string){
+	
+	/* Trim off leading whitespace */
+	while(*string == ' '){
+		string++;
+	}
+
+	/*TODO: Trim off trailing whitespace*/
+
+	return string;
+
 }
 
 #endif /* OCTO_H */
--- unittests.c
+++ unittests.c
@@ -4,9 +4,14 @@
 int main( void ){
 
 	printf("Unit testing has begun\n");
+
 	/* len() test */
 	char* testy = "BUNZ! ";
 	printf("The string \"%s\" is %d characters long.\n", testy, len(testy));
-	if(len(testy) == 6) printf("TEST PASSED.");
+	if(len(testy) == 6) printf("TEST PASSED.\n");
+
+	/* strip() test*/
+	char* testy2 = "       NARC!";
+	printf("The stripped string: \"%s\"\n",strip(testy2));
 	return 0;
 }
